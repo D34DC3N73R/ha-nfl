5a1808d19e596607eecec61989780c32e7549e3b
D34DC3N73R <D34DC3N73R@users.noreply.github.com>
2023-05-10 22:56:15 -0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,6 @@ import asyncio
 pytest_plugins = ("pytest_homeassistant_custom_component", "pytest_asyncio")
 
 
-@pytest.fixture(autouse=True)
+@pytest_asyncio.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
     yield
